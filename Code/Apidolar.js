bd6c114b3750050f144ecfb58617cd30684d6d41
Cesar Alejandro Torres Nuñez <esteysertorres2@gmail.com>
2024-11-10 03:40:00 -0400

    Update Apidolar.js

--- Code/Apidolar.js
+++ Code/Apidolar.js
@@ -1,18 +1,29 @@
 fetch("https://ve.dolarapi.com/v1/dolares/oficial")  
-    .then(response => response.json())  
-    .then(data => {  
-      
-      let arrayData = Object.values(data);
-      let Dolar = arrayData[4];
-      console.log(Dolar);
+  .then(response => response.json())  
+  .then(data => {  
+    let arrayData = Object.values(data);  
+    let Dolar = arrayData[4];
+    console.log(Dolar);  
 
-      let text = document.querySelector('.dolar')
+    let text = document.querySelector('.dolar');  
+    text.textContent = Dolar;  
 
-    text.textContent = Dolar ;
+    let xd = document.getElementById("BS");  
+    let btn = document.getElementById("btn");  
+    let h2 = document.getElementById("h2");  
 
-    })
-    .catch(error => console.log('sin conecion'));
-    
+    btn.addEventListener("click", () => {  
+      let Cantidad = parseFloat(xd.value); 
+      let resultado = calcular(Dolar, Cantidad); 
+      h2.textContent = resultado.toFixed(2) +' BS'; 
+    });  
+
+    function calcular(Dolar, Cantidad) {  
+      return Cantidad * Dolar;  
+    }  
+
+  })  
+  .catch(error => console.log('sin conexión'));
 
 const fechaUTC = new Date("2024-11-07T20:00:33.708Z");  
 const fechaVenezuela = new Date(fechaUTC.getTime() - (1 * 60 * 60 * 1000));  
