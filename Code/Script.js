6013119d3f268b9443c8b30d3450b21802f7a6a3
Cesar Alejandro Torres Nuñez <esteysertorres2@gmail.com>
2024-11-11 00:19:35 -0400

    Update Script.js

--- Code/Script.js
+++ Code/Script.js
@@ -1,18 +1,33 @@
 
+document.addEventListener('DOMContentLoaded', () => {  
+  const SS = document.getElementById('ss');  
+  const BS = document.getElementById('BS');  
+  const op = document.getElementById('Tranformar');  
+
+ 
+  holapulto();  
+
+ 
+  op.addEventListener('change', holapulto);  
+
+  function holapulto() {  
+      const seltOP = op.value;
+      console.log("Opción seleccionada:", seltOP);
+
+      
+      if (seltOP === "$$ a BS") {  
+          console.log("Seleccionado:$$ a BS");  
+          SS.style.display = 'none';  
+          BS.style.display = 'block';  
+      } else if (seltOP === "BS a $$") {  
+          console.log("Seleccionado: BS a $$");  
+          BS.style.display = 'none';  
+          SS.style.display = 'block';  
+      } else {  
+          console.log("No se seleccionó una opción válida.");  
+      }  
+  }  
 
 
-console.log(hola);
-const DolarText = document.querySelectorAll('.dolar');
 
-// Assuming you have a way to fetch the current dollar rate
-const Dolar = 4.5; // Replace this with the actual fetched value
-
-function Dolarhoy(dolar) {
-  return dolar;
-}
-
-DolarText.forEach(element => {
-  element.textContent = Dolarhoy(Dolar);
 });
-
-console.log(Dolar);
